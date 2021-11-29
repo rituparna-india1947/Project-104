c8e1a2a6eb41221549e75da37f3f0debbb88bcca
rituparna-india1947 <85564017+rituparna-india1947@users.noreply.github.com>
2021-11-29 21:24:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90,
+});
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+function snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML= '<img id= "selfie" src= "'+data_uri+'">';
+    });
+}
+
+console.log("ml5 version: ", ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/o568NHPIb/model.json', modelLoaded)
+function modelLoaded() {
+    console.log("modelLoaded")
+}
